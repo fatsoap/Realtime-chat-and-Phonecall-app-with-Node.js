8f18ca2635f6005a02743d68d849a0238fbd0609
fatsoap <g8sigma@icloud.com>
2021-04-03 15:38:46 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('https://soap-realtime-chat.herokuapp.com/');
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
   secure: true,
